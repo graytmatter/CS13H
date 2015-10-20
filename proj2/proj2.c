3dfee5a44be6486ba3554f0b8bbfa67992d5c201
graytmatter <matt.t.gray@gmail.com>
2015-10-20 09:31:22 -0700

    back to basics to test makefile in bsd

--- proj2/proj2.c
+++ proj2/proj2.c
@@ -4,15 +4,15 @@
 int main(int argc, char const *argv[])
 {
 
-  static char buffer[BUF_SIZE];
+  // static char buffer[BUF_SIZE];
 
-  printf("%s\n", "input word: ");
-  while(fgets(buffer, BUF_SIZE, stdin) != NULL){
-    printf("buffer: %s",buffer);
+  // printf("%s\n", "input word: ");
+  // while(fgets(buffer, BUF_SIZE, stdin) != NULL){
+  //   printf("buffer: %s",buffer);
 
-    printf("%s\n", "input word: ");
-  }
-  return 0;
+  //   printf("%s\n", "input word: ");
+  // }
+  // return 0;
 }
 
 
