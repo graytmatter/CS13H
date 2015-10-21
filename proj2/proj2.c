5ca56c041d2324bd035d471c5bf3e5d0bfd2ae8a
graytmatter <matt.t.gray@gmail.com>
2015-10-21 00:13:13 -0700

    done

--- proj2/proj2.c
+++ proj2/proj2.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #define BUF_SIZE 20
 // #include <nodes.h>
 
 char*getWord(char*buffer){
   printf("\n%s\n", "input word: ");
+  // fgets(buffer, BUF_SIZE, stdin);
+  // printf("%s\n",fgets(buffer, BUF_SIZE, stdin));
+  // if(buffer != NULL){
+  //   for(int i = 0; buffer[i]; i++){
+  //     buffer[i] = tolower(buffer[i]);
+  //   }
+  // }
   return fgets(buffer, BUF_SIZE, stdin);
 }
 
@@ -38,7 +46,7 @@ void printList(NODE*list){
 }
 
 int insertBasic(NODE *list, char *word) {
-  int cmpCount = 0;
+  int cmpCount = 1;
   NODE* foundnode = list;
   while(foundnode != NULL && strcmp(foundnode->word,word)!=0){
     cmpCount ++;
@@ -56,7 +64,7 @@ int insertBasic(NODE *list, char *word) {
 }
 
 int insertMTFR(NODE *list, char *word) {
-  int cmpCount = 0;
+  int cmpCount = 1;
   NODE* foundnode = list;
   while(foundnode != NULL && strcmp(foundnode->word,word)!=0){
     cmpCount ++;
@@ -64,14 +72,14 @@ int insertMTFR(NODE *list, char *word) {
   }
   if(foundnode != NULL){
     NODE* newList = foundnode;
-    if(newList->next){
-      newList->next->prev = newList->prev;
-    }
-    if(newList->prev){
-      newList->prev->next = newList->next;
-      newList->next = list;
-      list->prev = newList;
-      list = newList;
+    if(newList->prev != NULL){ //if it's already at front I don't need to do anything.
+      if(newList->next != NULL){ //if it's at the back we don't need to change the NULLS prev pointer
+        newList->next->prev = newList->prev; //if it's in the middle switch the next's prev pointer
+      }
+      newList->prev->next = newList->next; //if it isn't in the front switch the prev's next pointer
+      newList->next = list; //put rest after our new first
+      list->prev = newList; //put first before new rest
+      newList->prev = NULL;
       printf("%s\n","moved to front" );
     }
     newList->count ++;
@@ -84,11 +92,21 @@ int insertMTFR(NODE *list, char *word) {
   return cmpCount;
 }
 
+int clearList(NODE* list){
+  int count = 1;
+  while(list->next != NULL){
+    count++;
+    list = list->next;
+    free(list->prev);
+  }
+  free(list);
+  return count;
+}
+
 
 
 int main(int argc, char const *argv[])
 {
-  printf( "Lower case of A is %c\n", tolower('A'));
   static char buffer[BUF_SIZE];
   if(getWord(buffer) != NULL){
     NODE* basicList = makeNode(buffer);
@@ -96,23 +114,19 @@ int main(int argc, char const *argv[])
     NODE* MTFRList = makeNode(buffer);
     int MTFRCmpCount = 0;
     while(getWord(buffer) != NULL){
-      // basicCmpCount = insertBasic(basicList,buffer);
-      // if(basicList->prev != NULL){
-      //   basicList = basicList->prev;
-      // }
-      MTFRCmpCount = insertMTFR(MTFRList,buffer);
+      basicCmpCount += insertBasic(basicList,buffer);
+      if(basicList->prev != NULL){
+        basicList = basicList->prev;
+      }
+      MTFRCmpCount += insertMTFR(MTFRList,buffer);
       if(MTFRList->prev != NULL){
         MTFRList = MTFRList->prev;
       }
-      // basicList = basic->list;
-      // basicList = makeNode(buffer);
-      // printf("%s\n", "basicList");
-      // printList(basicList);
-      printf("%s\n", "MTFRList");
-      printList(MTFRList);
     }
-  printList(basicList);
 
+    //When reading the assignment I saw that it didn't ask me to print the frequency of the words, that's why I structured my end prints this way. I think this is the right format though I'm not sure.
+      printf("basicListInsert took %i steps and had %i unique words\n",basicCmpCount,clearList(basicList));
+      printf("MTFRListInsert took %i steps and had %i unique words \n",MTFRCmpCount,clearList(MTFRList));
   }
   return 0;
 }
