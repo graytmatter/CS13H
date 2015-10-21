d198717cef096f26618e41122665c2a25a54546c
graytmatter <matt.t.gray@gmail.com>
2015-10-20 23:20:22 -0700

    got basicList working

--- proj2/proj2.c
+++ proj2/proj2.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define BUF_SIZE 20
 // #include <nodes.h>
+
+char*getWord(char*buffer){
+  printf("\n%s\n", "input word: ");
+  return fgets(buffer, BUF_SIZE, stdin);
+}
+
 typedef struct node NODE;
 struct node{
   char word[BUF_SIZE];
@@ -10,78 +17,101 @@ struct node{
   NODE *prev;
 };
 
-NODE makenode(char*str){
-  NODE result;
-  strcpy(result.word,str);
-  result.count = 1;
+NODE* makeNode(char*str){
+  NODE* result = (NODE*)malloc(sizeof(NODE));
+  strcpy(result->word,str);
+  result->count = 1;
+  result->next = NULL;
+  result->prev = NULL;
   return result;
 }
-int main(int argc, char const *argv[])
-{
-  static char buffer[BUF_SIZE];
-  printf("%s\n", "input word: ");
-  // NODE = f
-  while(fgets(buffer, BUF_SIZE, stdin) != NULL){
-    NODE list = makenode(buffer);
-    printf("%s\n", "p5");
-    printf("buffer: %s\n",list.word);
 
-    printf("%s\n", "input word: ");
+void printList(NODE*list){
+  printf("%s\n","printing list" );
+  if(list == NULL){
+    printf("%s\n", "(NULL)");
   }
-  return 0;
+  while(list != NULL){
+    printf("word:%s count: %i \n", list->word,list->count);
+    list = list->next;
+  }
 }
-// typedef struct node NODE;
-// struct node
-// {
-//    char  title[50];
-//    char  author[50];
-//    char  subject[100];
-//    int   book_id;
-//    NODE *next;
-// };
 
-// int main( )
-// {
-//    BOOK Book1;        /* Declare Book1 of type Book */
-//    BOOK Book2;        /* Declare Book2 of type Book */
+int insertBasic(NODE *list, char *word) {
+  int cmpCount = 0;
+  NODE* foundnode = list;
+  while(foundnode != NULL && strcmp(foundnode->word,word)!=0){
+    cmpCount ++;
+    foundnode = foundnode->next;
+  }
+  if(foundnode != NULL){
+    foundnode->count ++;
+  }else{
+    NODE* newList = makeNode(word);
+    newList->next = list;
+    list->prev = newList;
+    list = newList;
+  }
+  return cmpCount;
+}
 
-//    /* book 1 specification */
-//    strcpy( Book1.title, "C Programming");
-//    strcpy( Book1.author, "Nuha Ali");
-//    strcpy( Book1.subject, "C Programming Tutorial");
-//    Book1.book_id = 6495407;
-
-//    /* book 2 specification */
-//    strcpy( Book2.title, "Telecom Billing");
-//    strcpy( Book2.author, "Zara Ali");
-//    strcpy( Book2.subject, "Telecom Billing Tutorial");
-//    Book2.book_id = 6495700;
-
-//    /* print Book1 info */
-//    printf( "Book 1 title : %s\n", Book1.title);
-//    printf( "Book 1 author : %s\n", Book1.author);
-//    printf( "Book 1 subject : %s\n", Book1.subject);
-//    printf( "Book 1 book_id : %d\n", Book1.book_id);
-
-//    /* print Book2 info */
-//    printf( "Book 2 title : %s\n", Book2.title);
-//    printf( "Book 2 author : %s\n", Book2.author);
-//    printf( "Book 2 subject : %s\n", Book2.subject);
-//    printf( "Book 2 book_id : %d\n", Book2.book_id);
-
-//    return 0;
+// int insertMTFR(NODE *list, char *word) {
+//   int cmpCount = 0;
+//   NODE* foundnode = list;
+//   while(foundnode != NULL && strcmp(foundnode->word,word)!=0){
+//     cmpCount ++;
+//     foundnode = foundnode->next;
+//   }
+//   if(foundnode != NULL){
+//     NODE* newList = foundnode;
+//     if(newList->prev){
+//       newList->prev->next = newList->next;
+//     }
+//     if(newList->next){
+//       newList->next->prev = newList->prev;
+//     }
+//     newList->count ++;
+//     newList->next = list;
+//     list->prev = newList;
+//     list = newList;
+//     printf("%s\n","moved to front" );
+//   }else{
+//     NODE* newList = makeNode(word);
+//     newList->next = list;
+//     list->prev = newList;
+//     list = newList;
+//   }
+//   return cmpCount;
 // }
 
 
 
+int main(int argc, char const *argv[])
+{
+  static char buffer[BUF_SIZE];
+  if(getWord(buffer) != NULL){
+    NODE* basicList = makeNode(buffer);
+    int basicCmpCount = 0;
+    NODE* MTFRList = makeNode(buffer);
+    int MTFRCmpCount = 0;
+    while(getWord(buffer) != NULL){
+      basicCmpCount = insertBasic(basicList,buffer);
+      if(basicList->prev != NULL){
+        basicList = basicList->prev;
+      }
+      // MTFRCmpCount = insertMTFR(MTFRList,buffer);
+      // if(MTFRList->prev != NULL){
+      //   MTFRList = MTFRList->prev;
+      // }
+      // basicList = basic->list;
+      // basicList = makeNode(buffer);
+      printf("%s\n", "basicList");
+      printList(basicList);
+      // printf("%s\n", "MTFRList");
+      // printList(MTFRList);
+    }
+  printList(basicList);
 
-
-// int x;
-// x = x+1;
-// &x = ....;
-// int* y = &x;
-// *y == x;
-
-// char* x;
-// x = ....;
-// *x = firstchar
+  }
+  return 0;
+}
